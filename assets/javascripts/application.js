07daf1a7b147141bdc42013e18acb63ec849a68f
darrengoffin <darren.goffin@me.com>
2021-04-13 13:05:15 +0100

    Update application.js

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,10 +1,3 @@
-// This source file is part of the Swift.org open source project
-//
-// Copyright (c) 2014 - 2017 Apple Inc. and the Swift project authors
-// Licensed under Apache License v2.0 with Runtime Library Exception
-//
-// See http://swift.org/LICENSE.txt for license information
-// See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
 
 /*! gumshoe v3.0.0 | (c) 2015 Chris Ferdinandi | MIT License | http://github.com/cferdinandi/gumshoe */
 !function(e, t) {
